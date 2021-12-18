9a7aa4cfed2cb799083c05a78941141ad1c9ead6
obiradige <akinfurkanerdogan@gmail.com>
2021-12-18 12:21:34 +0300

    create lazycolumn and get data from api

--- app/src/main/java/com/example/cryptocurrencywithcompose/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencywithcompose/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.cryptocurrencywithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cryptocurrencywithcompose.model.CryptoModel
@@ -32,6 +42,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
+
+    var cryptoModel = remember {
+        mutableStateListOf<CryptoModel>()
+    }
     val BASE_URL = "https://raw.githubusercontent.com/"
 
     val retrofit = Retrofit.Builder()
@@ -49,9 +63,7 @@ fun MainScreen(){
         ) {
             if(response.isSuccessful){
                 response.body()?.let {
-                    it.forEach {
-                        println(it)
-                    }
+                    cryptoModel.addAll(it)
                 }
             }
         }
@@ -62,7 +74,7 @@ fun MainScreen(){
 
     })
     Scaffold(topBar = {AppBar()}) {
-        
+        CryptoList(cryptos = cryptoModel)
     }
 }
 
@@ -70,6 +82,31 @@ fun MainScreen(){
 fun AppBar(){
     TopAppBar(contentPadding = PaddingValues(12.dp)) {
         Text(text = "First Compose Exercies")
+    }
+}
+
+@Composable
+fun CryptoList(cryptos : List<CryptoModel>){
+    LazyColumn(contentPadding = PaddingValues(12.dp) ){
+        items(cryptos) {crypto ->
+            CryptoRow(crypto = crypto)
+
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(crypto : CryptoModel){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colors.surface)) {
+        Text(text = crypto.currency, style = MaterialTheme.typography.h4,
+        modifier = Modifier.padding(2.dp),
+        fontWeight = FontWeight.Bold)
+        Text(text = crypto.price, style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold)
+        
     }
 }
 
